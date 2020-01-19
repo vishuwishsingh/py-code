d34d07efe2e212c75bfd1875bac719101f8d4d12
Vishva jeet Singh <vishuwish.singh@gmail.com>
2020-01-19 17:42:52 -0600

    Pandas

---
+++ Interactive_English_Dictionary_SQ_PG/source.py
@@ -0,0 +1,21 @@
+import mysql.connector
+
+con = mysql.connector.connect(
+user = "ardit700_student",
+password = "",
+host = "",
+database = ""
+)
+
+cursor = con.cursor()
+
+word=input("Enter the word: ")
+
+query = cursor.execute("SELECT Definition FROM Dictionary WHERE Expression = '%s'" % word)
+results = cursor.fetchall()
+
+if results:
+    for result in results:
+        print(result[0])
+else:
+    print("No word found!")
