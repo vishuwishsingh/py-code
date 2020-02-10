4cb20dd9038de9434612471bab1bff4097d8c8a6
Vishva jeet Singh <vishuwish.singh@gmail.com>
2020-02-09 18:25:37 -0600

    Book Pro

--- Interacting_with_Databases/script1_PostgreSQL.py
+++ Interacting_with_Databases/script1_PostgreSQL.py
@@ -1,20 +1,22 @@
-import py
+import psycopg2
 
 
-"""
+
 def create_table():
     
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect("dbname = 'database1' user='postgres' password='12345678' host='127.0.0.1' port='5499'" )
     cur = conn.cursor()
     cur.execute("CREATE TABLE IF NOT EXISTS store(item TEXT , quantity INTEGER , price REAL)")
     conn.commit()
     conn.close()
 
+create_table()    
+
 def insert_record(item , qty , price):
 
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect("dbname = 'database1' user='postgres' password='12345678' host='127.0.0.1' port='5499'" )
     cur = conn.cursor()
-    cur.execute("INSERT INTO store VALUES(?, ? , ?)",(item , qty , price))
+    cur.execute("INSERT INTO store VALUES(%s, %s , %s)",(item , qty , price))
     conn.commit()
     conn.close()
 
@@ -22,7 +24,7 @@ def insert_record(item , qty , price):
 #insert_record("Coffee Glass",10 , 10.5)
 
 def view():
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect("dbname = 'database1' user='postgres' password='12345678' host='127.0.0.1' port='5499'" )
     cur = conn.cursor()
     cur.execute("SELECT * FROM store")
     rows = cur.fetchall()
@@ -30,24 +32,25 @@ def view():
     return rows
 
 def delete(item):
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect("dbname = 'database1' user='postgres' password='12345678' host='127.0.0.1' port='5499'" )
     cur = conn.cursor()
     #lite3.ProgrammingError: Incorrect number of bindings supplied. The current statement uses 1, and there are 10 supplied
     #add comma afetr item
-    cur.execute("DELETE FROM store WHERE item = ? " ,(item,))
+    cur.execute("DELETE FROM store WHERE item = %s " ,(item,))
     conn.commit()
     conn.close
   
 
-delete('Wine Glass')
+
 
 def upd(quantity , price , item ):
-    conn = sqlite3.connect("lite.db")
+    conn = psycopg2.connect("dbname = 'database1' user='postgres' password='12345678' host='127.0.0.1' port='5499'" )
     cur = conn.cursor()
-    cur.execute("UPDATE  store SET quantity =? , price  = ? WHERE item = ?" ,(quantity , price , item ))
+    cur.execute("UPDATE  store SET quantity =%s , price  = %s WHERE item = %s" ,(quantity , price , item ))
     conn.commit()
     conn.close
 
-upd(20,20.5, 'Water Glass')
+delete('Water Glass')
+upd(20,20.5, 'Coffee Glass')
 print(view())
-"""
+
